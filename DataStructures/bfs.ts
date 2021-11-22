d9606db83e35a46ece124c2ba853e63c28e46455
Konstantin Ashaev <kostya@gorgaz.ru>
2021-11-22 14:38:06 +0300

    BT Level Order Traversal

---
+++ DataStructures/bfs.ts
@@ -0,0 +1,45 @@
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+
+function levelOrder(root: TreeNode | null): number[][] {
+  const res: number[][] = [];
+  let level: number = 0;
+
+  if (!root.val) return res;
+
+  const travers = (node: TreeNode, level: number) => {
+    if ((node.left || node.right) && !res[level]) res[level] = [];
+    if (node.left) res[level].push(node.left.val);
+    if (node.right) res[level].push(node.right.val);
+  
+    if (node.left) travers(node.left, level+1);
+    if (node.right) travers(node.right, level+1);
+  };
+
+  res[level] = [];
+  res[level].push(root.val);
+  level++;
+
+  travers(root, level);
+  return res;
+}
+
+const ex1 = new TreeNode(3, new TreeNode(9, null, null), 
+                         new TreeNode(20, new TreeNode(15, null, null), 
+                                     new TreeNode(7, null, null))
+                        );                          // [[3], [9, 20], [15, 7]]
+const ex2 = new TreeNode(1);                       // [[1]]
+const ex3 = new TreeNode();                       // [] 
+
+console.log(levelOrder(ex1));
+console.log(levelOrder(ex2));
+console.log(levelOrder(ex3));
