5f1778f59469a66c2b7d3a6f766839996a6f9d84
Konstantin Ashaev <kostya@gorgaz.ru>
2021-12-08 10:36:17 +0300

    Correct implementation. Accepted on leetcode

--- DataStructures/BinaryTree/isSymmetric.ts
+++ DataStructures/BinaryTree/isSymmetric.ts
@@ -12,20 +12,28 @@ class TreeNode {
 
  function isSymmetric(root: TreeNode | null): boolean {
    let res: boolean = false;
+   let left: number[] = [];
+   let right: number[] = [];
 
+   if (!root.right && !root.left) return res = true;
    if (!root.right || !root.left) return res;
 
    const travers = (node1: TreeNode, node2: TreeNode) => {
-     if (node1.val === node2.val) res = true;
-     else {
-       res = false;
-       return;
-     }
+     node1.val === node2.val ? res = true : res = false;
+     left  = [];
+     right = [];
+
+     node1.left ? left.push(1) : left.push(0);
+     node1.right ? left.push(1) : left.push(0);
+     node2.left ? right.push(1) : right.push(0);
+     node2.right ? right.push(1) : right.push(0);
+
+     if (left[0] != right[1]) res = false;
+     if (left[1] != right[0]) res = false;
+
+     if (node1.left && node2.right && res ) travers(node1.left, node2.right);
+     if (node1.right && node2.left && res) travers(node1.right, node2.left);
      
-     if (node1.left && node2.right) travers(node1.left, node2.right);
-     if (node1.right && node2.left) travers(node1.right, node2.left);
-     if (node1.left && !node2.right) res = false;
-     if (node1.right && !node2.left) res = false;
    };
 
    travers(root.left, root.right);
@@ -37,5 +45,8 @@ const ex1 = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)),
                          new TreeNode(2, new TreeNode(4), new TreeNode(3))); 
 const ex2 = new TreeNode(1, new TreeNode(2, null, new TreeNode(3)),
                          new TreeNode(2, null, new TreeNode(3)));
-console.log(isSymmetric(ex1));
-console.log(isSymmetric(ex2));
+const ex3 = new TreeNode(2, new TreeNode(3, new TreeNode(4), new TreeNode(5)),
+                         new TreeNode(3, new TreeNode(5)));
+console.log(isSymmetric(ex1)); // true
+console.log(isSymmetric(ex2)); // false
+console.log(isSymmetric(ex3)); // false
